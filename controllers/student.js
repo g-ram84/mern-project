d85933e1c1f2682db239670ba99dc88eece089d7
Graeme Chalmers <chalmers.graeme@gmail.com>
2021-05-12 09:51:45 -0700

    server side delete function

--- controllers/student.js
+++ controllers/student.js
@@ -1,3 +1,4 @@
+import student from "../models/student.js";
 import StudentData from "../models/student.js";
 
 export const getStudents = async (req, res) => {
@@ -20,5 +21,16 @@ export const createStudent = async (req, res) => {
 		res.status(201).json(newStudent);
 	} catch (error) {
 		res.status(409).json({ message: error.message });
+	}
+};
+
+export const deleteStudent = async (req, res) => {
+	const id = req.params.id;
+
+	try {
+		await StudentData.findByIdAndRemove(id).exec();
+		res.send("Successfully Deleted");
+	} catch (error) {
+		console.log(error);
 	}
 };
